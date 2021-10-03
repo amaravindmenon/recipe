e0fb092abf5133b2c3b9165c2e8d0dee4f5f9475
VISHESHK27 <61914430+visheshk27@users.noreply.github.com>
2021-10-03 17:33:56 +0530

    Update recipe.py

--- recipe.py
+++ recipe.py
@@ -9,11 +9,18 @@ def get_recipe(a):
         print("Milk Shake")
         print("Tea")
         print("coffee")
+    elif (a == 4):
+        print("Michelade")
+        print("Bloddy Mary")
+        print("coffee")
         
 print("SELECT YOUR Ingredient")
 print("1 for Sugar")
 print("2 for Salt")
 print("3 for Milk")
+print("4 for Tomato")
+print("5 for Blueberry")
+print("6 for ")
 x = int(input("Enter Number: "))
 get_recipe(x)
  
@@ -30,4 +37,40 @@ elif rec == "Salty Lemon":
     print("Add Salt + Water + Lemon crushed or powder")
 elif rec == "Milk Shake":
     print("Add Milk + Flavour like chocolate or strawberry")
-    
+elif rec == "Bloddy ria":
+    print('''1 To Taste Chili Lime Seasoning
+3⅓ Parts Tomato Juice
+1⅓ Parts Lime Juice
+1 Teaspoon Hot Sauce
+1 Teaspoon Soy Sauce'''')
+elif rec == "Bloddy Mary":
+    print('''1 To Taste Chili Lime Seasoning
+3⅓ Parts Tomato Juice
+1⅓ Parts Lime Juice
+1 Teaspoon Hot Sauce
+1 Teaspoon Soy Sauce'''')
+elif rec == "Bloddy John":
+    print('''1 To Taste Chili Lime Seasoning
+3⅓ Parts Tomato Juice
+1⅓ Parts Lime Juice
+1 Teaspoon Hot Sauce
+1 Teaspoon Soy Sauce'''')
+elif rec == "Bloddy tommy":
+    print('''1 To Taste Chili Lime Seasoning
+3⅓ Parts Tomato Juice
+1⅓ Parts Lime Juice
+1 Teaspoon Hot Sauce
+1 Teaspoon Soy Sauce'''')
+elif rec == "Michelade":
+    print('''1 Part Horseradish
+1½ Parts Absolut Vodka
+4 Parts Tomato Juice
+4 Dashes Hot Sauce
+⅓ Part Worcestershire Sauce
+⅓ Part Lemon Juice
+1 Wedge Lemon
+1 Stick Celery''')
+elif rec == "Milk Shake":
+    print("Add Milk + Flavour like chocolate or strawberry")
+elif rec == "Milk Shake":
+    print("Add Milk + Flavour like chocolate or strawberry")
